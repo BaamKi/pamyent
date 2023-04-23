abb881fde82538071716c9a812acaf69d709f6ff
Baamki <sevenbumki@gmail.com>
2023-04-24 02:03:23 +0900

    Header 추가

--- src/main/java/com/baam/payment/controller/PaymentController.java
+++ src/main/java/com/baam/payment/controller/PaymentController.java
@@ -8,10 +8,7 @@ import com.baam.payment.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,11 +29,17 @@ public class PaymentController {
 
     @PostMapping("/payment")
     @Timer
-    public String payment(@RequestBody PaymentRequestDTO paymentRequestDTO)
+    public String payment(@RequestBody PaymentRequestDTO paymentRequestDTO, @RequestHeader String authorization)
     {
-        logger.info(paymentService.payment(paymentRequestDTO).toString());
-        paymentService.payment(paymentRequestDTO);
-        return "hello";
+        if (authorization.equals("beom"))
+        {
+            logger.info(paymentService.payment(paymentRequestDTO).toString());
+            paymentService.payment(paymentRequestDTO);
+            return "hello";
+        }
+        return "다시 입력해";
+
+
     }
 
 
